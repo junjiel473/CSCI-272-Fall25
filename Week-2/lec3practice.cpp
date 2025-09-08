8a886910417f99fca989b33d5cca0f6038ea784b
Junjie Li <junjie.li@jjay.cuny.edu>
2025-09-08 10:35:31 -0400

    Add files via upload

---
+++ Week-2/lec3practice.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+// Example 1
+
+//template <typename T>
+//T maxNum(T a, T b){
+//	return (a > b) ? a : b;
+//}
+
+//int main (){
+//	int intMax = maxNum (5,10);
+//	double doubleMax = maxNum (2.5,11.6);
+//	
+//	cout << "Int Max: " << intMax << endl;
+//	cout << "Double Max: " << doubleMax << endl;
+//	
+//	return 0;
+//}
+
+// Example 2
+
+//template <typename T>
+//T mathFunction (T a, T b, T c){
+//	return a+b-c;
+//}
+//
+//int main (){
+//	int intCalc = mathFunction(20,30,40);
+//	double doubleCalc = mathFunction(11.11,22.22,33.00);
+//
+//	cout << "Integer = " << intCalc << endl;
+//	cout << "Double = " << doubleCalc << endl;
+//
+//}
+
+// Vector 
+
+//#include <vector>
+//using namespace std;
+//
+//int main(){
+//	vector<double> prices = {1,2,3,4,5};
+//	
+//	// adds values to the end of "prices" vector
+//	prices.push_back(10);
+//	prices.push_back(50);
+//	prices.push_back(11);
+//	
+//	double total = prices[0] + prices[1] + prices[2];
+//	cout << "Total = $" << total << endl;
+//	
+//	prices.insert(prices.(), 20);
+//	
+//	cout << "First item: " << prices.front() << endl;
+//	cout << "Last item: " << prices.back() << endl;
+//	cout << "Total size of my vector (prices): " << prices.size() << endl;
+//	
+//	double total2 = prices[0] + prices[1] + prices[2];
+//	
+//	cout << "Total = $" << total2 << endl;
+//	
+//	return 0;
+//}
